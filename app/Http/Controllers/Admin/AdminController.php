1520aba7099c3f4dbfe5b6310688da8c7e9cf70f
Ivan <ivanmelo47@gmail.com>
2024-10-09 23:44:46 -0600

    second_commit

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Helpers\ResponseHelper; // Importar la clase Helper
 
 class AdminController extends Controller
 {
-    //
+    public function usuarios(Request $request)
+    {
+        // Obtener la cabecera 'Authorization'
+        $header = $request->header('Authorization');
+
+        $usuarios = DB::table('users')->get();
+
+        return response()->json([
+            'status' => true,
+            'validation' => true,
+            'code' => 200,
+            'message' => 'Usuarios',
+            'notifications' => ['Usuarios'],
+            'data' => $usuarios,
+            'header' => $header,
+        ], 200);
+
+        return ResponseHelper::jsonResponse(true, true, 200, 'Usuarios', ['Usuarios'], $usuarios);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\AdminController;
 
-// Ruta para registrar un nuevo usuario
-Route::post('/register', [AuthController::class,'register']);
+// API'S VERSION 1
+Route::prefix('v1')->group(function () {
+    // Ruta para registrar un nuevo usuario
+    Route::post('/register', [AuthController::class, 'register']);
 
-// Ruta para iniciar sesión
-Route::post('/login', [AuthController::class, 'login']);
+    // Ruta para iniciar sesión
+    Route::post('/login', [AuthController::class, 'login']);
 
-// Rutas protegidas (requieren autenticación)
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    // Agrega aquí otras rutas que necesiten autenticación
-    Route::get('/user', [AuthController::class, 'getUser']); // Ejemplo
+    // Rutas protegidas (requieren autenticación)
+    Route::middleware('auth:sanctum')->group(function () {
+
+        // Ruta para cerrar SESION
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        // Rutas de administración con prefijo "admin"
+        Route::prefix('admin')->group(function () {
+            // Ruta para obtener la lista de usuarios
+            Route::post('/users', [AdminController::class, 'usuarios']);
+        });
+    });
 });
-

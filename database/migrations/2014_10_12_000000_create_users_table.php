afe9c571d59c63b040cf708e108134536741bfc2
Ivan <ivanmelo47@gmail.com>
2024-10-10 22:05:35 -0600

    migracion_update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('uuid', 100)->unique();
+            $table->string('personal_token', 50);
             $table->string('username', 50)->unique();
-            $table->string('url_img', 80);
+            $table->string('url_img', 80)->nullable();
             $table->string('email', 80)->unique();
             $table->string('password', 100);
             $table->string('role', 20)->nullable()->default('user');
